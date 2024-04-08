5688291a2f9a6affed7588425704de7360557f70
Diana024 <154326533+Diana024@users.noreply.github.com>
2024-04-08 13:26:53 +0000

    Exercise Family API

--- src/app.py
+++ src/app.py
@@ -2,43 +2,51 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
-from flask_cors import CORS
-from utils import APIException, generate_sitemap
+from flask import Flask, request, jsonify, url_for  # type: ignore
+from flask_cors import CORS # type: ignore
+# from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+from utils import generate_sitemap
+# from models import Person
+
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
-jackson_family = FamilyStructure("Jackson")
+jackson_family = FamilyStructure('Jackson')
 
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
+# # Handle/serialize errors like a JSON object
+# @app.errorhandler(APIException) # type: ignore
+# def handle_invalid_usage(error):
+#     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+# Obtener GET todos los miembros de la familia
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
+
+# Recupera solo un miembro id == member_id
+@app.route('/member/<int:id>', methods=['GET'])
+def get_members_id(id):
+    members = jackson_family.get_member(id)
+    return jsonify(members), 200
+
+#  Añadir (POST) un miembro
 
 
-    return jsonify(response_body), 200
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
+    PORT = int(os.environ.get('PORT', 3000)) # type: ignore
     app.run(host='0.0.0.0', port=PORT, debug=True)
